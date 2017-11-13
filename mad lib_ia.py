62c45c0f9c43c973f5d395886ef227ad6b1dcbaa
IsabelA10 <33633096+IsabelA10@users.noreply.github.com>
2017-11-13 11:48:25 -0500

    First upload

---
+++ mad lib_ia.py
@@ -0,0 +1,62 @@
+### MAD LIBS ###
+### Source: http://www.woojr.com/wp-content/uploads/2010/05/funny-mad-libs.gif
+### Ask questions here ###
+print ("Write an adjective.")
+adj1 = input()
+
+print ("Write a nationality.")
+nat1 = input()
+
+print ("Name a person.")
+person1 = input()
+
+print ("Write a noun.")
+noun1 = input()
+
+print ("Write an adjective.")
+adj2 = input()
+
+print ("Write a noun.")
+noun2 = input()
+
+print ("Write an adjective.")
+adj3 = input()
+
+print ("Write an adjective.")
+adj4 = input()
+
+print ("Write a plural noun.")
+plnoun1 = input()
+
+print ("Write a noun.")
+noun3 = input()
+
+print ("Write a number.")
+number1 = input()
+
+print ("Write a shape.")
+shape1 = input()
+
+print ("Name a food.")
+food1 = input()
+
+print ("Name a food.")
+food2 = input()
+
+print ("Name a number.")
+number2 = input()
+
+
+
+### Tell the MadLib ###
+print ("Pizza was invented by a " + adj1 +" "+ nat1 + " chef named " + person1 + ".")
+print ("To make a pizza, you need to take a lump of " + noun1 + ", and make a thin, round " + adj2 + noun2 + ".")
+print ("Then you cover it with " + adj3 + " sauce, " + adj4 + " cheese, and fresh chopped " + plnoun1 + ".")
+print ("Next you have to bake it in a very hot " + noun3 + ".")
+print ("When it is done, cut it into " + number1 +" "+ shape1 + ".")
+print ("Some kids like " + food1 + " pizza the best, but my favorite is the " + food2 + " pizza.")
+print ("If I could, I would eat pizza " + number2 + " times a day!")
+
+
+
+
---
+++ personality_survey_IA.py
@@ -0,0 +1,40 @@
+print ("What's your favorite sport?")
+sport = input ()
+
+if sport == "soccer":
+    print("That's my favorite too!")
+elif sport == "dance":
+    print("I love dance!")
+elif sport == "tennis":
+    print("I love playing doubles!")
+
+else:
+    print(sport + " sounds fun.")
+
+
+print("What's your favorite ice cream flavor?")
+flavor = input()
+
+if flavor == "sea salt caramel oreo":
+    print ("I love that one! What toppings do you like?")
+    toppings = input()
+    if toppings == "rainbow sprinkles and strawberries":
+        print ("Love those! But my favorite are reeses pieces!")
+elif flavor == "chocolate fudge":
+    print ("I hate that one! What toppings do you even eat with that?")
+    toppings = input()
+    if toppings == "rainbow sprinkles and strawberries":
+        print ("Love those! But my favorite are reeses pieces!")
+    elif toppings == "chocolate sprinkles":
+        print ("rainbow sprinkles are way better!")
+
+
+print ("What's your favorite froyo flavor?")
+froyo = input()
+
+if froyo == "coconut":
+    print ("Im alergic to coconut!")
+elif froyo == "cookies and cream":
+    print ("That's my favourite!")
+        
+
